18eaab7bb63c1aae252393c9c1700a9642ef3d2b
Shane Keulen <shane.keulen@gmail.com>
2014-10-23 13:54:10 -0400

    save

--- filter.js
+++ filter.js
@@ -1,3 +1,16 @@
-$(function() {
+(function ($) {
+  $.fn.createFilter = function(options) {
+    return this;
+  };
 
+}(jQuery));
+
+$(function() {
+  function createFakeData(amount) {
+    var result = [];
+    for (i=0; i < amount; i++)result.push(faker.name.findName());
+    return result;
+  };
+  var fakeArray = createFakeData(10);
+  console.table(fakeArray);
 });
